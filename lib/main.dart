4b40538f38ebdc3152c3b32edda6255d3b3ca010
Osama <osamaahmed70088@gmail.com>
2024-12-06 08:51:17 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          useMaterial3: false,
+        ),
+        home: const Scaffold());
+  }
+}
