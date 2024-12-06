653af3ddf9dc85c6863bb6bdb3fe2a84df9c6cec
Osama <osamaahmed70088@gmail.com>
2024-12-06 09:53:19 +0300

    added Padding Wdiget to the

--- lib/views/widgets/home_view_body.dart
+++ lib/views/widgets/home_view_body.dart
@@ -6,8 +6,13 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [CustomAppBarWidget()],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          CustomAppBarWidget(),
+        ],
+      ),
     );
   }
 }
