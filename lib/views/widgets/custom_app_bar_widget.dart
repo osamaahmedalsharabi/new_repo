9578a4e81b16d0e44f851bdb4090db52f0c78f29
Osama <osamaahmed70088@gmail.com>
2024-12-06 09:17:45 +0300

    create custom app bar widget file

---
+++ lib/views/widgets/custom_app_bar_widget.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class CustomAppBarWidget extends StatelessWidget {
+  const CustomAppBarWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [],
+    );
+  }
+}
